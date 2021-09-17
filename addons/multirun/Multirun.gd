678283a91a7fabd569399f8b7d1e32533f7e2682
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-09-17 05:45:17 -0400

    Rewrote the multirun plugin to better support my use case

--- addons/multirun/Multirun.gd
+++ addons/multirun/Multirun.gd
@@ -3,6 +3,7 @@ extends EditorPlugin
 
 var panel1
 var panel2
+var panel3
 var pids = []
 
 func _enter_tree():
@@ -12,21 +13,57 @@ func _enter_tree():
 	var icon_transition_auto = gui_base.get_icon("TransitionSyncAuto", "EditorIcons")
 	var icon_load = gui_base.get_icon("Load", "EditorIcons")
 	
-	panel2 = _add_tooblar_button("_loaddir_pressed", icon_load, icon_load)
-	panel1 = _add_tooblar_button("_multirun_pressed", icon_transition, icon_transition_auto)
-	
-	_add_setting("debug/multirun/number_of_windows", TYPE_INT, 2)
-	_add_setting("debug/multirun/window_distance", TYPE_INT, 1270)
-	_add_setting("debug/multirun/add_custom_args", TYPE_BOOL, true)
-	_add_setting("debug/multirun/first_window_args", TYPE_STRING, "listen")
-	_add_setting("debug/multirun/other_window_args", TYPE_STRING, "join")
+	panel1 = _add_texture_button("_loaddir_pressed", icon_load, icon_load)
+	panel2 = _add_text_button("_server_pressed", 'server')
+	panel3 = _add_text_button("_client_pressed", 'client')
 
-func _multirun_pressed():
-	var window_count : int = ProjectSettings.get_setting("debug/multirun/number_of_windows")
-	var window_dist : int = ProjectSettings.get_setting("debug/multirun/window_distance")
-	var add_custom_args : bool = ProjectSettings.get_setting("debug/multirun/add_custom_args")
-	var first_args : String = ProjectSettings.get_setting("debug/multirun/first_window_args")
-	var other_args : String = ProjectSettings.get_setting("debug/multirun/other_window_args")
+	_add_setting("debug/multirun/server/number_of_windows", TYPE_INT, 3)
+	_add_setting("debug/multirun/server/window_distance", TYPE_INT, 1270)
+	_add_setting("debug/multirun/server/add_custom_args", TYPE_BOOL, true)
+	_add_setting("debug/multirun/server/first_window_args", TYPE_STRING, "--server=1")
+	_add_setting("debug/multirun/server/other_window_args", TYPE_STRING, "--connect=1")
+
+	_add_setting("debug/multirun/client/number_of_windows", TYPE_INT, 2)
+	_add_setting("debug/multirun/client/window_distance", TYPE_INT, 1270)
+	_add_setting("debug/multirun/client/add_custom_args", TYPE_BOOL, true)
+	_add_setting("debug/multirun/client/first_window_args", TYPE_STRING, "--connect=1")
+	_add_setting("debug/multirun/client/other_window_args", TYPE_STRING, "--server=1")
+
+
+func _server_pressed():
+	var window_count : int = ProjectSettings.get_setting("debug/multirun/server/number_of_windows")
+	var window_dist : int = ProjectSettings.get_setting("debug/multirun/server/window_distance")
+	var add_custom_args : bool = ProjectSettings.get_setting("debug/multirun/server/add_custom_args")
+	var first_args : String = ProjectSettings.get_setting("debug/multirun/server/first_window_args")
+	var other_args : String = ProjectSettings.get_setting("debug/multirun/server/other_window_args")
+	var commands = ["--position", "500,0"]
+	if first_args && add_custom_args:
+		for arg in first_args.split(" "):
+			commands.push_front(arg)
+
+	var main_run_args = ProjectSettings.get_setting("editor/main_run_args")
+	if main_run_args != first_args:
+		ProjectSettings.set_setting("editor/main_run_args", first_args)
+	var interface = get_editor_interface()
+	interface.play_main_scene()
+	if main_run_args != first_args:
+		ProjectSettings.set_setting("editor/main_run_args", main_run_args)
+
+	kill_pids()
+	for i in range(window_count-1):
+		commands = ["--position", str(50 + (i+1) * window_dist) + ",10"]
+		if other_args && add_custom_args:
+			for arg in other_args.split(" "):
+				commands.push_front(arg)
+		pids.append(OS.execute(OS.get_executable_path(), commands, false))
+
+
+func _client_pressed():
+	var window_count : int = ProjectSettings.get_setting("debug/multirun/client/number_of_windows")
+	var window_dist : int = ProjectSettings.get_setting("debug/multirun/client/window_distance")
+	var add_custom_args : bool = ProjectSettings.get_setting("debug/multirun/client/add_custom_args")
+	var first_args : String = ProjectSettings.get_setting("debug/multirun/client/first_window_args")
+	var other_args : String = ProjectSettings.get_setting("debug/multirun/client/other_window_args")
 	var commands = ["--position", "50,10"]
 	if first_args && add_custom_args:
 		for arg in first_args.split(" "):
@@ -67,17 +104,29 @@ func _remove_panels():
 	if panel2:
 		remove_control_from_container(CONTAINER_TOOLBAR, panel2)
 		panel2.free()
+	if panel3:
+		remove_control_from_container(CONTAINER_TOOLBAR, panel3)
+		panel3.free()
 
 func _unhandled_input(event):	
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_F4:
-			_multirun_pressed()
+			_server_pressed()
 
-func _add_tooblar_button(action:String, icon_normal, icon_pressed):
+func _add_texture_button(action:String, icon_normal, icon_pressed):
 	var panel = PanelContainer.new()
 	var b = TextureButton.new();
 	b.texture_normal = icon_normal
 	b.texture_pressed = icon_pressed
+	b.connect("pressed", self, action)
+	panel.add_child(b)
+	add_control_to_container(CONTAINER_TOOLBAR, panel)
+	return panel
+
+func _add_text_button(action:String, text):
+	var panel = PanelContainer.new()
+	var b = Button.new();
+	b.text = text
 	b.connect("pressed", self, action)
 	panel.add_child(b)
 	add_control_to_container(CONTAINER_TOOLBAR, panel)
